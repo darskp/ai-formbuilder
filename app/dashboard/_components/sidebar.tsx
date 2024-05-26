f7ebb0da2fed8cc56f45c4497bdb5718a69ad5d4
darskp <darshanpatgar100@gmail.com>
2024-05-26 16:19:23 +0530

    form setup done

--- app/dashboard/_components/sidebar.tsx
+++ app/dashboard/_components/sidebar.tsx
@@ -7,19 +7,16 @@ import React, { useEffect } from 'react'
 
 const Sidebar = (): JSX.Element => {
     const path = usePathname();
-    useEffect(() => {
-        console.log(path);
-    }, [path])
 
     return (
         <div className='h-screen shadow-md border-r'>
             <div className='p-4'>
-                {menuList.map((menu, index, arr) =>
-                    <h2 key={menu.id}
+                {menuList?.map((menu, index, arr) =>
+                    <h2 key={menu?.id}
                         className={`flex items-center gap-3 text-gray-500
-                    p-4 mb-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer ${path == menu.path && `bg-primary text-white`}`}>
+                    p-4 mb-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer ${path == menu?.path && `bg-primary text-white`}`}>
                         <menu.icon />
-                        {menu.name}
+                        {menu?.name}
                     </h2>
                 )}
             </div>
--- app/edit-form/_components/formUi.tsx
+++ app/edit-form/_components/formUi.tsx
@@ -9,11 +9,12 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import { Checkbox } from '@/components/ui/checkbox';
-import { Loader2 } from 'lucide-react';
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
+import FieldEdit from './FieldEdit';
 
-const FormUi = ({ jsonFormData }: { jsonFormData: any }) => {
+const FormUi = ({ jsonFormData, onUpdate, onDeleteFormField }: { jsonFormData: any, onUpdate: (value: any, index: number) => void, onDeleteFormField:(index:number)=>void }) => {
     console.log(jsonFormData);
+
 
     return (
         <div className='border p-5 rounded-lg lg:w-[600px] '>
@@ -21,9 +22,9 @@ const FormUi = ({ jsonFormData }: { jsonFormData: any }) => {
             <h2 className='text-sm text-center text-gray-400'>{jsonFormData?.formSubheading}</h2>
             <div>
                 {jsonFormData?.formFields?.map((field: any, index: number) => (
-                    <div key={index} className='my-3'>
+                    <div key={index} className='my-3 flex gap-2 items-center'>
                         {field?.fieldType == 'select' ?
-                            <div>
+                            <div className='w-full'>
                                 <Label htmlFor={field?.fieldName} className='text-xs mb-1 text-gray-500'>{field?.fieldLabel}</Label>
                                 <div className='mt-1 '>
                                     <Select>
@@ -39,7 +40,7 @@ const FormUi = ({ jsonFormData }: { jsonFormData: any }) => {
                                 </div>
                             </div>
                             : (field?.fieldType == 'checkbox') ?
-                                <div className="flex items-center space-x-2">
+                                <div className="w-full flex items-center space-x-2">
                                     <Checkbox id={field?.fieldName} />
                                     <Label
                                         htmlFor={field?.fieldName}
@@ -49,7 +50,7 @@ const FormUi = ({ jsonFormData }: { jsonFormData: any }) => {
                                     </Label>
                                 </div> :
                                 (field?.fieldType == 'radio') ?
-                                    <div>
+                                    <div className='w-full'>
                                         <Label htmlFor={field?.fieldName} className='text-xs text-gray-500'>{field?.fieldLabel}</Label>
                                         <RadioGroup className='mt-1'>
                                             {field?.options?.map((item: any, index: number) => (
@@ -61,7 +62,7 @@ const FormUi = ({ jsonFormData }: { jsonFormData: any }) => {
                                         </RadioGroup>
                                     </div>
                                     :
-                                    <div>
+                                    <div className='w-full'>
                                         <Label htmlFor={field?.fieldName} className='text-xs text-gray-500'>{field?.fieldLabel}</Label>
                                         <Input
                                             className='mt-1 w-full focus-visible:ring-2 focus-visible:ring-primary'
@@ -70,9 +71,11 @@ const FormUi = ({ jsonFormData }: { jsonFormData: any }) => {
                                             type={field?.fieldType}
                                             placeholder={field?.placeholder}
                                         />
-
                                     </div>
                         }
+                        <div className='flex items-center justify-center'>
+                            <FieldEdit defaultValue={field} onUpdate={(value) => onUpdate(value, index)} onDeleteFormField={() => onDeleteFormField(index)}/>
+                        </div>
                     </div>
                 ))}
             </div>
